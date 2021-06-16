7c00cafde989d27027c4c4c1d057f4fc0a836a4c
Anton Korelskiy <korantonkor@list.ru>
2021-06-16 14:15:45 +0300

    Draw thumbnails of images

--- js/photo.js
+++ js/photo.js
@@ -1,1 +1,19 @@
 // Просмотр загруженных изображений.
+import {customPhotos} from './data.js';
+
+const listPictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesMiniature = customPhotos;
+const picturesListFragment = document.createDocumentFragment();
+
+picturesMiniature.forEach(({url, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  picturesListFragment.appendChild(pictureElement);
+});
+
+listPictures.appendChild(picturesListFragment);
+
+
